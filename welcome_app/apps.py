41a45c27c6e39e838d2da5f1c7ed9007aa22a1da
Paul Jones <pjones@ivycollegiate.org>
2023-05-14 08:52:49 +0000

    creates welcome_app

--- welcome_app/apps.py
+++ welcome_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WelcomeConfig(AppConfig):
+class WelcomeAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "welcome"
+    name = "welcome_app"
